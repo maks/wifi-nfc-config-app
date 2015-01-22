a933da63f5ef118341df7be4a675d55564e65983
Maksim Lin <maks@manichord.com>
2015-01-22 20:23:34 +1100

    fix name in makefile to match github repo name

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := nfc-wifi-app
+LOCAL_PACKAGE_NAME := wifi-nfc-config-app
 LOCAL_CERTIFICATE := platform
 
 #LOCAL_PROGUARD_FLAG_FILES := proguard.flags
